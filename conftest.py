6e6c6c8d626470424509b1e9e9a9c23415ab05a4
kamiye <kamiye31@gmail.com>
2026-05-05 19:39:59 +0100

    Set browser to headless mode in page fixture for test execution

--- conftest.py
+++ conftest.py
@@ -20,7 +20,7 @@ def test_password():
 @pytest.fixture(scope="function")
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True) 
         context = browser.new_context()
         context.set_default_timeout(15000)
 
